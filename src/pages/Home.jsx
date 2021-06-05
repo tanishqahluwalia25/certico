ae598b4e893664536d7504da7872dd74faceff9c
TanishqAhluwalia25 <tanishqwalia25@gmail.com>
2021-06-05 15:33:22 +0530

    Added certificate1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import Cert from "../components/Cert"
+import Cert from "../components/Cert";
+import Cert1 from "../components/Cert1";
 
 const Home = () => {
-    return (
-        <div>
-            <Cert></Cert>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Cert1 />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
