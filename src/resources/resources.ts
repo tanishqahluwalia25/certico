b90e311f7ae42ef93cbfc8f446f133b4ec8c0517
shadowMonarchCode <rishuprakash2002@gmail.com>
2021-06-23 12:45:18 +0530

    Added assets | Background-2 & 3

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -7,5 +7,7 @@ export enum Colors {
 
 export const Images = {
     cert_illus: require('../assets/cert_illus.svg'),
-    hero_bg_lg: require('../assets/hero_bg_lg.svg')
+    hero_bg_lg: require('../assets/hero_bg_lg.svg'),
+    background_2: require('../assets/Background_2.svg'),
+    background_3: require('../assets/Background_3.svg')
 }
